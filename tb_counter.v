946fc89ac880009d34f353af8bd8aa500d4c2985
udaijain-vlsi <udaijain2025@gmail.com>
2026-02-05 22:07:37 +0530

    tb_counter.v

--- tb_counter.v
+++ tb_counter.v
@@ -1,1 +1,18 @@
+module tb_counter;
+reg clk, rst;
+wire [3:0] cnt;
+counter dut (.clk(clk), .rst(rst), .cnt(cnt));
+initial begin
+clk = 0;
+forever #5 clk = ~clk;
+end
+initial begin
+rst = 1;     
+#15 rst = 0; 
+#200 $finish; 
+end
+always @(posedge clk) begin
+if (cnt >= 16) $display("ERROR: Counter overflow!");
+end
+endmodule
 
